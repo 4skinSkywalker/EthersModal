cb4f7252b7bedb907ab531a5838d5e58eba43ab4
4skinSkywalker <stopchemtrailsfred@gmail.com>
2021-10-12 13:20:14 +0200

    Fix path to script for demo

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
+let path = require("path");
+
 module.exports = {
     entry: {
-        'main': "./src/index.js"
+        main: "./src/index.js"
     },
     output: {
         filename: "./main.js",
-        library: ["EthersModal"]
+        library: ["EthersModal"],
+        path: path.resolve(__dirname, "docs")
     }
 };
